43ffb325a315a61beeb52e6f33bc316cfc663865
Adrian Pena <adrianpena@arkham.lan>
2023-11-05 13:41:11 -0500

    Deleted csv and txt files in repo

--- RandomPractice.py
+++ RandomPractice.py
@@ -26,3 +26,16 @@ for i in range(5):
     t = icecream["toppings"][order[1]]
     s = icecream["sizes"][order[2]]
     print("Order " + str(i+1) + ": Flavor: " + f + ", Topping: " + t + ", Size: " + s)
+
+# # Given a string str. You are allowed to delete only some contiguous characters if all the characters are the same in a single operation. The task is to find the minimum number of operations required to completely delete the string. 
+
+# Examples:
+
+# Input: str = “abcddcba” 
+
+# Output: 4 Delete dd, then the string is “abccba” Delete cc, then the string is “abba” Delete bb, then the string is “aa” Delete aa, then the string is null. 
+
+# Input: str = “abc” 
+
+# Output: 3 
+
